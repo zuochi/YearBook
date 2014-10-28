2f16fefea35ebbd59bf2db62af52bb819847f911
zuochi <313212272@qq.com>
2014-10-28 13:28:30 +0800

    2

--- WebRoot/js/User_index.js
+++ WebRoot/js/User_index.js
@@ -102,7 +102,7 @@ createXMLHttpRequest();
 							$("#photosUL2").append(
 								"<li value='"+json[i].id+"'>"+
 									"<div id='comment"+json[i].id+"' class='text2'>"+
-										"<textarea rows='3' cols='50' id='reply"+json[i].id+"' onfocus='friendNameTipsInit("+json[i].id+")' onkeyup='getAtName(this.value.charAt(value.length-1),"+json[i].id+")'></textarea>"+
+										"<textarea rows='3' cols='50' id='reply"+json[i].id+"' onfocus='friendNameTipsInit("+json[i].id+")' onkeyup='getAtName(this.value.charAt(value.length-1),"+json[i].id+")'></textarea>"+"<br>"+
 										"<input type='button' value='Reply' onclick='comment("+document.getElementById("userId").value+","+json[i].id+")'/>"+
 										"<div class='ds-post-main'>"+
 											"<div class='ds-avatar'>"+
