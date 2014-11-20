91f6ba2855eec537657a2a465620fcf598bc0e1d
zuochi <313212272@qq.com>
2014-11-20 12:36:49 +0800

    文本框的哪个位置都能艾特！！！！！！！！！！！！！！！！跟微博的区别。。我的还会飘一瓢。。不过不影响使用。。

--- WebRoot/js/at.js
+++ WebRoot/js/at.js
@@ -1,24 +1,27 @@
 		//当用户输入@的时候 寻找用户好友列表里面的相关人员
 		var searchFriendNameStatus = 0;
 		var atFriendName;
+		var friendNameLength = 0;
 		var selectIndex = 0;
 		var selectLength = 0;
 		var atIndex = 0;
-		function getAtName(text,photoId){
+		function getAtName(context,photoId){
+			var cursorPos = getCursorPos("reply"+photoId);
+			var text = context.charAt(cursorPos-1);
+			var afterText = context.charAt(cursorPos);
 			//统计剩余字数
 			wordsNumber(photoId);
-			//删除前后空格
-	        //text.replace(/(^\s*)|(\s*$)/g,"");
-			if(text=="@"){
+			//判断当前输入的字符是否为@，且判断@后面是否结尾或者空格。
+			if(text=="@" && (afterText==" " || afterText=="")){
 				searchFriendNameStatus = 1;
 				atFriendName = "";
 				//记录@的位置
-				atIndex = getCursorPos("reply"+photoId);
+				atIndex = cursorPos;
 				$("#friendTips"+photoId).html("<center>Who do you want to at?</center>");
 				$("#friendTips"+photoId).css("display","block");
 			}
 			if(searchFriendNameStatus==1){
-				if(text==" "){
+				if(window.event.keyCode==32){
 					searchFriendNameStatus=0;
 					$("#friendTips"+photoId).html("");
 					$("#friendTips"+photoId).css("display","none");
@@ -26,6 +29,8 @@
 				}else if(text!="@"){
 					//上选择
 					if(window.event.keyCode==38){
+						//保持不变
+						setCursorPos("reply"+photoId, atIndex+atFriendName.length);
 						$("#friendTips"+photoId).css("display","block");
 						if(selectIndex>=1){
 							$("#ft"+photoId+selectIndex).css("color","white");
@@ -40,6 +45,8 @@
 					}
 					//下选择
 					if(window.event.keyCode==40){
+						//保持不变
+						setCursorPos("reply"+photoId, atIndex+atFriendName.length);
 						$("#friendTips"+photoId).css("display","block");
 						if(selectIndex<(selectLength-1)){
 							$("#ft"+photoId+selectIndex).css("color","white");
@@ -63,8 +70,8 @@
 							return;
 						}
 					}else{
-						atFriendName += text;
-						atFriendName = $("#reply"+photoId).val().substr(atIndex,atFriendName.length);
+						//atFriendName += text;
+						atFriendName = $.trim($("#reply"+photoId).val().substring(atIndex,getCursorPos("reply"+photoId)));
 					}
 					$.ajax({
 						url:'/YearBook/user/getSearchInfomation_execute',  
@@ -115,20 +122,38 @@
 		//去除回车
 		function removeEnter(photoId){
 			$("#reply"+photoId).val($("#reply"+photoId).val().replace(/[\r\n]/g,""));
+			//保持光标位置
+			setCursorPos("reply"+photoId, atIndex+friendNameLength);
 		};
 		
 		//自动填充
 		function autoComplete(photoId,friendName){
 			var value = $("#reply"+photoId).val();
-			value = value.substring(0,(value.length-atFriendName.length));
-			value+=(friendName+" ");
-			$("#reply"+photoId).val(value);
+			
+			var currentIndex = atIndex+atFriendName.length;
+			
+			var head = value.substring(0,currentIndex-atFriendName.length);
+			
+			var end = value.substring(currentIndex+atFriendName.length);
+			
+			//value+=(friendName+" ");
+			if($.trim(end)==''){
+				$("#reply"+photoId).val(head + friendName);
+			}else{
+				$("#reply"+photoId).val(head + friendName + " " + end);
+			}
 			//重置搜索状态
 			searchFriendNameStatus=0;
 			$("#friendTips"+photoId).html("");
 			$("#friendTips"+photoId).css("display","none");
 			//光标置后,并获取焦点
-			focusLast(document.getElementById("reply"+photoId));
+			//focusLast(document.getElementById("reply"+photoId));
+			//保持光标位置
+			setCursorPos("reply"+photoId, atIndex+friendName.length);
+			//清空用户名
+			atFriendName = "";
+			//记录完整用户名长度
+			friendNameLength = friendName.length;
 			//统计剩余字数
 			wordsNumber(photoId);
 		};
@@ -188,6 +213,8 @@
 	            textRange.collapse();
 	            textRange.select();
 	        }else{
-	            inpObj.setSelectionRange(n,n);
+	            inpObj.setSelectionRange(pos,pos);
+	        	//inpObj.selectionStart = inpObj.selectionEnd = pos;
+	        	inpObj.focus();
 	        }
 	    };
