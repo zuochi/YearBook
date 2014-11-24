3ca72b68d57c317b688c2bf999d8b8ff43b7eb64
zuochi <313212272@qq.com>
2014-11-24 15:04:04 +0800

    去掉自动回复后面的冒号，只跟一个空格算了。

--- WebRoot/js/getReply.js
+++ WebRoot/js/getReply.js
@@ -72,7 +72,7 @@ function showComments(photoBid,toPageC){
 
 //评论自动回复填写
 function replyAutoComplete(nickName,photoBid){
-	$("#reply"+photoBid).val("reply@" + nickName+" : ");
+	$("#reply"+photoBid).val("reply@" + nickName+" ");
 	$("#reply"+photoBid).focus();
 	focusLast(document.getElementById(("reply"+photoBid)));
 };
