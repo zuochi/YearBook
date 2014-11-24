46a2a14e8c3f3947c72320dab088a68f889ca420
zuochi <313212272@qq.com>
2014-11-24 15:58:22 +0800

    删除无用代码。

--- src/action/user/GetSearchInfomation.java
+++ src/action/user/GetSearchInfomation.java
@@ -1,6 +1,5 @@
 package action.user;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.sf.json.JSONArray;
@@ -30,15 +29,6 @@ public class GetSearchInfomation extends UserAction{
 				
 				List<FriendInfomation> friendNameList = service.getDtoObjectsBySql("select u.name,h.url_s from user u LEFT JOIN head_photo h on u.head_photo_id=h.id where u.is_delete=0 and u.id in (select f.friend_id from friend_list f where f.user_id=" + user.getId() + ") and u.name like '" + friendList.getUserByFriendId().getName() + "%'", pc, new FriendInfomation());
 				
-				/*List<FriendList> friendLists = service.getObjectsByHql("from FriendList where isDelete=0 and userByUserId.id="+user.getId() + " and userByFriendId.name like '"+friendList.getUserByFriendId().getName()+"%'", pc);
-				
-				List<FriendInfomation> friendNameList = new ArrayList<FriendInfomation>();
-				for(FriendList f : friendLists){
-					User friend = (User) service.getObjectByHql("from User where id="+f.getUserByFriendId().getId(), "getHeadPhoto");
-					if(friend!=null){
-						friendNameList.add(new FriendInfomation(friend.getHeadPhoto().getUrlS(),friend.getName()));
-					}
-				}*/
 				JSONArray json = JSONArray.fromObject(friendNameList);
 				out.print(json);
 			}
