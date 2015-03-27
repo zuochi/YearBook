2f0a5224284d291be9f0fdfb9e93138991cd3d3f
Yodomi Jinta <Yodomi Jinta@Yodomi-Jinta>
2015-03-28 00:48:19 +0800

    fix bug!

--- src/action/admin/ReviewFeedBacks.java
+++ src/action/admin/ReviewFeedBacks.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import bean.FeedBack;
+import bean.User;
 
 @Controller
 @Scope("prototype")
@@ -18,10 +19,18 @@ public class ReviewFeedBacks extends AdminAction{
 		try {
 			out = response.getWriter();
 			
+			User user = (User) session.get("user");
+			
+			//TODO 过滤器
+			if(user==null){
+				return "login";
+			}
+			
 			FeedBack feedBackOld = (FeedBack) service.getObjectByHql("from FeedBack where isDelete=0 and id=" + feedBack.getId());
 			if(feedBackOld!=null){
 				//修改审核结果
 				feedBackOld.setStatus(feedBack.getStatus());
+				feedBackOld.setUserDeal(user);
 				//添加审核时间
 				if(feedBack.getStatus()==0){
 					feedBackOld.setDealDate(null);
